0775cb0c754e70c0be6e450b778105ee76169c57
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-08 17:01:25 -0400

    completed exercise 0

---
+++ exercises.py
@@ -0,0 +1,29 @@
+fav_colours = ["blue", "red", "maroon", "gray"]
+age = [28, 27, 26, 24]
+coin_flip = ["tails", "tails", "heads", "heads", "heads"]
+fav_artist = ["Simple Plan", "Bruno Mars", "Carly Rae Jepsen"]
+
+words = {
+    "cheesy": "Things done or said to make someone feel special",
+    "Cloud": "My dog",
+    "Kuya": "My brother",
+    }
+
+movies = {
+    "The Lion King": 1994,
+    "Isn't It Romantic": 2019,
+    "Deadpool": 2016,    
+}
+
+cities = {
+    "Winnipeg": 749534,
+    "Steinbach": 15829,
+    "Toronto": 2930000,
+}
+
+people = {
+    "Jasmin": 28,
+    "Jeannine": 27,
+    "Dylan": 26,
+    "Mollin": 24,
+    }
